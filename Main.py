b3b9b153869f701d397bfb5118673b46a61bcb1b
rohan.mehta <mehta08rohan@gmail.com>
2019-12-10 14:41:04 +0530

    Second Commit. Added Uploaded Button

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_file ,request, flash
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -13,10 +14,15 @@ def download_file():
     path = '\data.txt'
     return send_file(path,as_attachment=True)
 
-
-
 #https://stackoverflow.com/questions/39801728/using-flask-to-load-a-txt-file-through-the-browser-and-access-its-data-for-proce
 
+@app.route('/upload')
+def upload_file():
+    if request.method == 'POST':
+        return "PoSTed"
+        # f = request.files['uploadedtxt']
+        # f.save(secure_filename(f.filename))
+        # return 'file uploaded successfully'
 
 
 if __name__=="__main__":
