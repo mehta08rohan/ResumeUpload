d0642b7d7c2f877de99478467f77b5a94b5e56c7
rohan.mehta <mehta08rohan@gmail.com>
2019-12-12 15:37:21 +0530

    Fourth Commit.Uploaded Functionality Working Fine.

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, send_file ,request, flash
 from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'C:\\Users\\rohan.mehta\\PycharmProjects\\ResumeUpload\\static\\txt\\'
@@ -14,8 +15,13 @@ def download_file():
     path = 'C:\\Users\\rohan.mehta\\PycharmProjects\\ResumeUpload\\data.txt'
     return send_file(path,as_attachment=True)
 
-@app.route('/upload')
+@app.route('/upload', methods=['GET','POST'])
 def upload_file():
+    if request.method == "POST":
+        f = request.files['file']
+        # f.save(secure_filename(f.filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        return render_template('my_form.html')
     return render_template('upload.html')
 
 
