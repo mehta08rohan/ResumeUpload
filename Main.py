212aaf95ac3d36b530d1fc4720b0dce6b0b8e663
rohan.mehta <mehta08rohan@gmail.com>
2019-12-13 09:53:09 +0530

    Fourth Commit.Uploaded Functionality Working Fine.

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, send_file ,request, flash
 from werkzeug.utils import secure_filename
 import os
+import datetime
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'C:\\Users\\rohan.mehta\\PycharmProjects\\ResumeUpload\\static\\txt\\'
@@ -20,16 +21,21 @@ def upload_file():
     if request.method == "POST":
         f = request.files['file']
         # f.save(secure_filename(f.filename))
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        # f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+        filename = f.filename.rsplit('.')
+        date = str(datetime.datetime.now())
+        date = date.replace('-', '').replace(':', '').replace('.', '').replace(' ','_')
+        filename = filename[0] + date + '.' + filename[1]
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(filename)))
 
-        with open(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)),'r') as my_file:
+        with open(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(filename)),'r') as my_file:
             lines = my_file.readlines()
             my_list =[]
             for i in lines:
                 my_list.append(i)
             return str(my_list)
 
-        # return render_template('my_form.html')
+        return render_template('my_form.html')
     return render_template('upload.html')
 
 
